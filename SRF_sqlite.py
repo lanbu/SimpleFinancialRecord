945d98bb81dc9a2e9fe8ac93648e7029965fe23a
lanbu <lanbucom@qq.com>
2018-03-10 19:18:21 +0800

    添加数据库sqlite操作，需完善

--- SRF_sqlite.py
+++ SRF_sqlite.py
@@ -10,19 +10,29 @@ import sqlite3
 class FinancialDataRecord():
 	#init
 	def __init__(self):
-		#create a connection
-		#self.conn = sqlite3.connect('SRF_RecordsDB.db')
+		#record table contents
+		self.usr_name = None
+		self.record_date = None
+		self.record_num = None
+		self.record_income = None
+		self.record_income_s = None
+		self.record_expense = None
+		self.record_expense_s = None
+		self.record_remark = None
+		#create a connection\
+		self.conn = sqlite3.connect('SRF_RecordsDB.db')
 		pass
 		
 	#create main table
 	def create_record_table(self):
-		#self.cursor = self.conn.cursor()
-		#self.cursor.execute('create table UserFinanRecords (usr_name varchar(20) primary key, file_path varchar(100))')
-		pass
-		
+		self.cursor = self.conn.cursor()
+		self.cursor.execute('''CREATE TABLE UserFinanRecords (usr_name TEXT, date TEXT, recordNum TEXT, income REAL, income_s TEXT, expense REAL, expense_s TEXT, remark TEXT)''')
+				
 	#insert one record
 	def insert_one_record(self):
-		pass
+		self.cursor.execute("INSERT INTO UserFinanRecords VALUES ('%s','%s','%s',%f, '%s', %f, '%s' '%s')" 
+							%(self.usr_name, self.record_date, self.record_num, self.record_income, self.record_income_s, self.record_expense, self.record_expense_s))
+		
 		
 	#delete one record
 	def delete_one_record(self):
@@ -30,3 +40,4 @@ class FinancialDataRecord():
 		
 	def close_sqlite(self):
 		pass
+		
